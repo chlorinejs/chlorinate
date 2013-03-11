92683e8b19069469e9ad108baf4fad5f909473cd
Hoang Minh Thang <p@banphim.net>
2013-03-11 19:32:34 +0700

    update to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject chlorinate "1.0.0"
   :description "Convert javascript to Clojure/Chlorine"
   :url "http://github.com/chlorinejs/chlorinate"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "4.0.3"]])
